ec001675bce1859926525280b02ab8bfdc6ff6aa
Tejas Gajjar <imtejasgajjar@gmail.com>
2024-04-21 16:31:29 +0530

    Added full code

---
+++ src/components/utils/Data.ts
@@ -0,0 +1,346 @@
+import {
+  Gamepad2,
+  Headphones,
+  Image,
+  LucideIcon,
+  Share2,
+  Shirt,
+  Camera,
+  ShoppingCart,
+  Ticket,
+} from 'lucide-react';
+
+export interface ISectionCardData {
+  id: number;
+  title: string;
+  src: string;
+  exp: string;
+  snippetCount: number;
+  progress: number;
+  href?: string;
+}
+
+export interface IProjectSectionCardData {
+  id: number;
+  imageUrl?: string;
+  icon?: string;
+  title: string;
+  techStack: string;
+  href?: string;
+}
+
+export const projectCard: IProjectSectionCardData[] = [
+  {
+    id: 1,
+    title: 'Outdoor Cooking Experts',
+    techStack: 'Woocommerce,WordPress,B2C,Multilingual',
+    imageUrl: 'main.jpg',
+    href: '#',
+  },
+  {
+    id: 2,
+    title: 'Nustone',
+    techStack:
+      'WooCommerce, CustomCalculator, CustomChat, Zoho, Typescript, Tailwind CSS',
+    imageUrl: 'Nustonemain.png',
+    href: '#',
+  },
+  {
+    id: 3,
+    title: 'Commercial and Legal',
+    techStack:
+      'Mean Stack, React JS, Mongoose, Next.js 13, Next Auth, React Hook Form, Typescript, Tailwind CSS',
+    imageUrl: 'main-5.png',
+    href: '#',
+  },
+  {
+    id: 4,
+    title: 'TaskWave',
+    techStack: 'OpenAI, Nodejs, Express, Mongoose, ReactJS, Tailwind CSS',
+    imageUrl: 'Group-4.jpg',
+    href: '',
+  },
+];
+
+export const moreProjects = [
+  {
+    id: 1,
+    title: 'Visonair',
+    techStack: 'WordPress,W',
+    icon: 'camera',
+    href: '',
+  },
+  {
+    id: 2,
+    title: 'Intellizy',
+    techStack: 'WordPress, Chaim,',
+    icon: 'turtle',
+    href: 'https://intellizy.co.nz/',
+  },
+  {
+    id: 3,
+    title: 'Dezayno',
+    techStack: 'WordPress, Rest API, React',
+    icon: 'shirt',
+    href: 'https://dezayno.com/',
+  },
+
+  {
+    id: 4,
+    title: 'Connetix tiles',
+    techStack: 'WordPress, Payment gatewat plugin with multiple account',
+    icon: 'toybrick',
+    href: 'https://connetixtiles.com/',
+  },
+  ,
+
+  {
+    id: 5,
+    title: 'PRM Management Group',
+    techStack: 'WordPress, Custom Web Systems, Website Design',
+    icon: 'manage',
+    href: 'https://prmgroup.com.au/',
+  },
+];
+
+export const frontendCard: ISectionCardData[] = [
+  {
+    id: 1,
+    title: 'HTML, CSS',
+    src: '/tech/frontend/htmlcss.jpeg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 90,
+  },
+  {
+    id: 2,
+    title: 'Tailwind',
+    src: '/tech/frontend/tailwind.jpg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 90,
+  },
+  {
+    id: 3,
+    title: 'Javascript, Typescript',
+    src: '/tech/frontend/jsts.jpeg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 85,
+  },
+  {
+    id: 4,
+    title: 'Reactjs',
+    src: '/tech/frontend/reactjs.jpg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+  {
+    id: 5,
+    title: 'Nextjs 13',
+    src: '/tech/frontend/nextjs13.jpeg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+];
+
+export const backendCard: ISectionCardData[] = [
+  {
+    id: 1,
+    title: 'NodeJs',
+    src: '/tech/backend/nodejs.webp',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 90,
+  },
+  {
+    id: 2,
+    title: 'ExpressJs',
+    src: '/tech/backend/express.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 90,
+  },
+  {
+    id: 3,
+    title: 'DenoJs',
+    src: '/tech/backend/denojs.webp',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 60,
+  },
+  {
+    id: 4,
+    title: 'Next Auth',
+    src: '/tech/backend/nextauth.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 90,
+  },
+  {
+    id: 5,
+    title: 'PHP',
+    src: '/tech/backend/php.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 70,
+  },
+];
+
+export const databaseCard: ISectionCardData[] = [
+  {
+    id: 1,
+    title: 'MySQL',
+    src: '/tech/database/mysql.jpeg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+  {
+    id: 2,
+    title: 'MongoDB',
+    src: '/tech/database/mongo.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+  {
+    id: 3,
+    title: 'Redis',
+    src: '/tech/database/redis.jpeg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 70,
+  },
+  {
+    id: 4,
+    title: 'PostgreSQL',
+    src: '/tech/database/postgres.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 70,
+  },
+  {
+    id: 5,
+    title: 'Elastic Search',
+    src: '/tech/database/elasticSearch.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 70,
+  },
+];
+
+export const devopsCards: ISectionCardData[] = [
+  {
+    id: 1,
+    title: 'Docker',
+    src: '/tech/devops/docker.webp',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+  {
+    id: 2,
+    title: 'Kubernetes',
+    src: '/tech/devops/k8s.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+  {
+    id: 3,
+    title: 'GIT',
+    src: '/tech/devops/git.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+  {
+    id: 4,
+    title: 'Jenkins',
+    src: '/tech/devops/jenkins.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 70,
+  },
+  {
+    id: 5,
+    title: 'Terraform',
+    src: '/tech/devops/terraform.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 60,
+  },
+];
+
+export const web3Cards: ISectionCardData[] = [
+  {
+    id: 1,
+    title: 'Solidity',
+    src: '/tech/web3/solidity.jfif',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 90,
+  },
+  {
+    id: 2,
+    title: 'Hardhat',
+    src: '/tech/web3/hardhat.jpeg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+  {
+    id: 3,
+    title: 'Metamask',
+    src: '/tech/web3/metamask.webp',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 90,
+  },
+  {
+    id: 4,
+    title: 'Chainlink',
+    src: '/tech/web3/chainlink.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 80,
+  },
+  {
+    id: 5,
+    title: 'OpenZeppelin',
+    src: '/tech/web3/openzeppelin.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 70,
+  },
+];
+
+export const otherCards: ISectionCardData[] = [
+  {
+    id: 1,
+    title: 'Rest API',
+    src: '/tech/others/rest.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 90,
+  },
+  {
+    id: 2,
+    title: 'SOAP API',
+    src: '/tech/others/soap.jpg',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 70,
+  },
+  {
+    id: 3,
+    title: 'GraphQl',
+    src: '/tech/others/graphql.png',
+    exp: '1 day ago',
+    snippetCount: 1128,
+    progress: 70,
+  },
+];
